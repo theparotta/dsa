d78068987d831a3ed9d9506f80b8386b654667bc
TheParotta <126277487+theparotta@users.noreply.github.com>
2023-10-16 06:24:05 +0530

    Update reverse_the_string.c

--- drills/reverse_the_string/reverse_the_string.c
+++ drills/reverse_the_string/reverse_the_string.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+ * Complexity Analysis:
+ *  Time Complexity  - O(N)  // as we loop through the entire array
+ *  Space Complecity - O(N)  // as we create new array to store reversed string
+*/
+
+void reverse_str(char[], char[], int);
+
 int main() {
   char str[100];
+  char reversed_str[100];
 
   printf("Enter an input string: ");
   fgets(str, 99, stdin);
+  
+  reverse_str(str, reversed_str, strlen(str));
+  puts(reversed_str);
 
-  unsigned long str_len = strlen(str);
-  printf("Size of the string: %lu\n", str_len);
-
-  /* reversing the string and storing it in a array */
-  char reversed_str[str_len];
-  for (int i=str_len-1, j=0; i >= 0; i--, j++) 
-    reversed_str[j] = str[i];
-
-  /* finally adding the string termination character */
-  reversed_str[str_len] = '\0';
-
-  printf("Reversed string: %s\n", reversed_str);
   return 0;
+}
+
+void reverse_str(char str[], char reversed_str[], int len) {
+    for (int i=len-1, j=0; i >= 0; i--, j++)
+        *(reversed_str + j) = *(str + i);
 }

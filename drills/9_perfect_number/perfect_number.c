79248d2019b06b25c6e655b9b86550753c6a27de
TheParotta <126277487+theparotta@users.noreply.github.com>
2023-12-14 13:54:41 +0530

    perfect_number c solution

---
+++ drills/9_perfect_number/perfect_number.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool is_perfect_number(int input) {
+  /* STEPS:
+   * ======
+   * 1. find all the factors of the input
+   *      a. find the square root of the number
+   *      b. setup loop till that number
+   *      c. check if each number divides the input, if yes then it's a factor
+   * 2. calculate the sum, excluding the number
+   * 3. check if it's equal to input or not
+  */
+
+  int sum = 0;
+
+  printf("\nFactors of %d: 1", input);
+
+  for (int i = 2; i < (int) sqrt(input) + 1; i++) {
+    if (input % i == 0) {
+      sum += i;
+      sum += (input / i);
+      printf(", %d, %d", i, input/i);
+    }
+  }
+
+  return (sum + 1) == input;
+}
+
+
+
+int main() {
+  int input;
+
+  printf("Enter a number to check perfect_number or not: ");
+  scanf("%d", &input);
+
+  bool status = is_perfect_number(input);
+
+  if (status) puts("\nIt's a PERFECT NUMBER");
+  else puts("\nIt's NOT A PERFECT NUMBER");
+
+  return(0);
+}
